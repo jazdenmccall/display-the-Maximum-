3928291b955c23a374c128672c2a42f5b64856c3
jazdenmccall <mccall.jazden0@gmail.com>
2023-03-17 00:05:29 -0500

    Add files via upload

---
+++ display_the_maximum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+// Write a program which takes two numbers from the 
+// console and displays the maximum of the two.
+
+public class DisplayTheMax
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a random number: ");
+        var temp = Console.ReadLine();
+        int Input1 = Convert.ToInt32(temp);
+
+        Console.WriteLine("Enter a random Secondary Number: ");
+        temp = Console.ReadLine();
+        int Input2 = Convert.ToInt32(temp);
+
+        var toPrint = (Input1 > Input2) ? "First number was bigger" : " \n Second number next to biggest";
+        Console.WriteLine(toPrint);
+
+        
+        
+    }
+}
